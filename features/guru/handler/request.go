d7d50f8cd51344ab994150f3493c72702dc6fc24
audizzy <zulfeekei@gmail.com>
2023-02-02 00:12:47 +0700

    feat: fixed request.go register struct

--- features/guru/handler/request.go
+++ features/guru/handler/request.go
@@ -3,24 +3,22 @@ package handler
 import "Gurumu/features/guru"
 
 type LoginRequest struct {
-	Username string `json:"username" form:"username"`
+	Nama     string `json:"username" form:"username"`
 	Password string `json:"password" form:"password"`
 }
 
 type RegisterRequest struct {
-	Username string `json:"username" form:"username"`
-	Fullname string `json:"fullname" form:"fullname"`
+	Nama     string `json:"nama" form:"nama"`
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
 }
 type UpdateRequest struct {
-	Username string `json:"username" form:"username"`
-	Fullname string `json:"fullname" form:"fullname"`
+	Nama     string `json:"nama" form:"nama"`
 	Password string `json:"password" form:"password"`
 	Email    string `json:"email" form:"email"`
 	City     string `json:"city" form:"city"`
-	Address  string `json:"address" form:"address"`
-	Phone    string `json:"phone" form:"phone"`
+	Alamat   string `json:"alamat" form:"alamat"`
+	Telepon  string `json:"telepon" form:"telepon"`
 }
 
 func ReqToCore(data interface{}) *guru.Core {
@@ -29,23 +27,20 @@ func ReqToCore(data interface{}) *guru.Core {
 	switch data.(type) {
 	case LoginRequest:
 		cnv := data.(LoginRequest)
-		res.Username = cnv.Username
+		res.Nama = cnv.Nama
 		res.Password = cnv.Password
 	case RegisterRequest:
 		cnv := data.(RegisterRequest)
 		res.Email = cnv.Email
-		res.Username = cnv.Username
-		res.Fullname = cnv.Fullname
+		res.Nama = cnv.Nama
 		res.Password = cnv.Password
 	case UpdateRequest:
 		cnv := data.(UpdateRequest)
-		res.Username = cnv.Username
-		res.Fullname = cnv.Fullname
+		res.Nama = cnv.Nama
 		res.Password = cnv.Password
 		res.Email = cnv.Email
-		res.City = cnv.City
-		res.Address = cnv.Address
-		res.Phone = cnv.Phone
+		res.Alamat = cnv.Alamat
+		res.Telepon = cnv.Telepon
 	default:
 		return nil
 	}

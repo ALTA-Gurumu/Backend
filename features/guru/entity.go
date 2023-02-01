7e3ad03c40de4b059c5c18482fa41d0314a5c92b
audizzy <zulfeekei@gmail.com>
2023-02-01 23:47:07 +0700

    feat: added entity.go, model.go

---
+++ features/guru/entity.go
@@ -0,0 +1,42 @@
+package guru
+
+import (
+	"mime/multipart"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Core struct {
+	ID        uint
+	Email     string `validate:"required,email"`
+	Password  string `validate:"required"`
+	Nama      string `validate:"required"`
+	Deskripsi string
+	Ijazah    string
+	Pelajaran string
+	Telepon   string
+	Alamat    string
+	Avatar    string
+	Role      string
+}
+
+type GuruHandler interface {
+	Register() echo.HandlerFunc
+	Profile() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+	Update() echo.HandlerFunc
+}
+
+type GuruService interface {
+	Register(newGuru Core) (Core, error)
+	Profile(token interface{}) (Core, error)
+	Update(token interface{}, updateData Core, avatar *multipart.FileHeader) (Core, error)
+	Delete(token interface{}) error
+}
+
+type GuruData interface {
+	Register(newGuru Core) (Core, error)
+	Profile(id uint) (Core, error)
+	Update(id uint, updateData Core) (Core, error)
+	Delete(id uint) error
+}

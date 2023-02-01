f9b3fc5d2938731476ca4996e9cfdf9aa97b3e1f
Herdiladania <herdiladania11@gmail.com>
2023-02-02 01:15:34 +0700

    feat : add login service func

---
+++ features/autentikasi/service/service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"Gurumu/features/autentikasi"
+	"Gurumu/helper"
+	"errors"
+	"log"
+	"strings"
+
+	"github.com/go-playground/validator"
+)
+
+type autentikasiUseCase struct {
+	qry autentikasi.AutentikasiData
+	vld *validator.Validate
+}
+
+func New(ad autentikasi.AutentikasiData) autentikasi.AutentikasiService {
+	return &autentikasiUseCase{
+		qry: ad,
+		vld: validator.New(),
+	}
+}
+func (aud *autentikasiUseCase) Login(email, password string) (string, autentikasi.Core, error) {
+	res, err := aud.qry.Login(email)
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "not found") {
+			msg = "data tidak ditemukan"
+		} else {
+			msg = "terdapat masalah pada server"
+		}
+		return "", autentikasi.Core{}, errors.New(msg)
+	}
+
+	if err := helper.CheckPassword(res.Password, password); err != nil {
+		log.Println("login compare", err.Error())
+		return "", autentikasi.Core{}, errors.New("password tidak sesuai " + res.Password)
+	}
+
+	token, _ := helper.GenerateJWT(int(res.ID))
+
+	return token, res, nil
+
+}

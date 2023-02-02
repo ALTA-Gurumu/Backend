d53eacf724710383c6111e8026ced28ea1d4fcce
audizzy <zulfeekei@gmail.com>
2023-02-03 00:03:05 +0700

    added register routing main.go

--- main.go
+++ main.go
@@ -42,6 +42,7 @@ func main() {
 	// e.DELETE("/users", userHdl.Delete(), middleware.JWT([]byte(config.JWT_KEY)))
 
 	e.POST("/guru", guruHdl.Register())
+	e.DELETE("/guru", guruHdl.Delete(), middleware.JWT([]byte(config.JWT_KEY)))
 	if err := e.Start(":8000"); err != nil {
 		log.Println(err.Error())
 	}

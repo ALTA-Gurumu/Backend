fb5f7a4f6ffc4f23ab260de3a05efda209592c64
Herdiladania <herdiladania11@gmail.com>
2023-02-03 03:50:47 +0700

    feat : add jadwal struct and convertion func

---
+++ features/jadwal/data/model.go
@@ -0,0 +1,57 @@
+package data
+
+import (
+	"Gurumu/features/jadwal"
+
+	"gorm.io/gorm"
+)
+
+type Guru struct {
+	gorm.Model
+	Email     string
+	Password  string
+	Nama      string
+	Telepon   string
+	Deskripsi string
+	Ijazah    string
+	Pelajaran string
+	Alamat    string
+	Avatar    string
+	Role      string
+	Jadwals   []Jadwal `gorm:"foreignKey:GuruID;references:ID"`
+}
+type Jadwal struct {
+	gorm.Model
+	GuruID  uint
+	Tanggal string
+	Jam     string
+	Status  string
+}
+
+func ToCore(data Jadwal) jadwal.Core {
+	return jadwal.Core{
+		ID:      data.ID,
+		GuruID:  data.GuruID,
+		Tanggal: data.Tanggal,
+		Jam:     data.Jam,
+		Status:  data.Status,
+	}
+}
+
+func CoreToData(data jadwal.Core) Jadwal {
+	return Jadwal{
+		Model:   gorm.Model{ID: data.ID},
+		GuruID:  data.GuruID,
+		Tanggal: data.Tanggal,
+		Jam:     data.Jam,
+	}
+}
+
+func ListToCore(data []Jadwal) []jadwal.Core {
+	listJadwal := []jadwal.Core{}
+	for _, jadwal := range data {
+		listJadwal = append(listJadwal, ToCore(jadwal))
+	}
+
+	return listJadwal
+}

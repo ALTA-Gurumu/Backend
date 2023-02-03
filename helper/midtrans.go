f83e3d370536e7bd04c50caa5f183d83f168b2e5
Herdiladania <herdiladania11@gmail.com>
2023-02-04 04:03:42 +0700

    feat : add midtrans coreapi config, createtransaction func, and checkpaymentstatus fun

---
+++ helper/midtrans.go
@@ -0,0 +1,121 @@
+package helper
+
+import (
+	"Gurumu/config"
+
+	"github.com/midtrans/midtrans-go"
+	"github.com/midtrans/midtrans-go/coreapi"
+)
+
+// Initiate coreapi client
+func MidtransCoreAPIClient() coreapi.Client {
+	c := coreapi.Client{}
+	c.New(config.MIDTRANSSERVERKEY, midtrans.Sandbox)
+	return c
+}
+
+// Initiate charge request
+func CreateReservasiTransaction(kodePembayaran string, totalTarif int, metodePembayaran string) *coreapi.ChargeResponse {
+	c := MidtransCoreAPIClient()
+
+	//Initiate charge request by payment method
+	switch {
+	//Bank BCA
+	case metodePembayaran == "transfer_va_bca":
+		chargeReq := &coreapi.ChargeReq{
+			PaymentType: "bank_transfer",
+			TransactionDetails: midtrans.TransactionDetails{
+				OrderID:  kodePembayaran,
+				GrossAmt: int64(totalTarif),
+			},
+			BankTransfer: &coreapi.BankTransferDetails{
+				Bank: midtrans.BankBca,
+			},
+			CustomExpiry: &coreapi.CustomExpiry{
+				ExpiryDuration: 12,
+				Unit:           "hour",
+			},
+		}
+		coreApiRes, _ := c.ChargeTransaction(chargeReq)
+		return coreApiRes
+	//Bank BRI
+	case metodePembayaran == "transfer_va_bri":
+		chargeReq := &coreapi.ChargeReq{
+			PaymentType: "bank_transfer",
+			TransactionDetails: midtrans.TransactionDetails{
+				OrderID:  kodePembayaran,
+				GrossAmt: int64(totalTarif),
+			},
+			BankTransfer: &coreapi.BankTransferDetails{
+				Bank: midtrans.BankBri,
+			},
+			CustomExpiry: &coreapi.CustomExpiry{
+				ExpiryDuration: 12,
+				Unit:           "hour",
+			},
+		}
+		coreApiRes, _ := c.ChargeTransaction(chargeReq)
+		return coreApiRes
+	//Bank BNI
+	case metodePembayaran == "transfer_va_bni":
+		chargeReq := &coreapi.ChargeReq{
+			PaymentType: "bank_transfer",
+			TransactionDetails: midtrans.TransactionDetails{
+				OrderID:  kodePembayaran,
+				GrossAmt: int64(totalTarif),
+			},
+			BankTransfer: &coreapi.BankTransferDetails{
+				Bank: midtrans.BankBni,
+			},
+			CustomExpiry: &coreapi.CustomExpiry{
+				ExpiryDuration: 12,
+				Unit:           "hour",
+			},
+		}
+		coreApiRes, _ := c.ChargeTransaction(chargeReq)
+		return coreApiRes
+	//Bank Permata
+	case metodePembayaran == "transfer_va_permata":
+		chargeReq := &coreapi.ChargeReq{
+			PaymentType: "bank_transfer",
+			TransactionDetails: midtrans.TransactionDetails{
+				OrderID:  kodePembayaran,
+				GrossAmt: int64(totalTarif),
+			},
+			BankTransfer: &coreapi.BankTransferDetails{
+				Bank: midtrans.BankPermata,
+			},
+			CustomExpiry: &coreapi.CustomExpiry{
+				ExpiryDuration: 12,
+				Unit:           "hour",
+			},
+		}
+		coreApiRes, _ := c.ChargeTransaction(chargeReq)
+		return coreApiRes
+
+	//QRIS (Gopay)
+	case metodePembayaran == "qris":
+		chargeReq := &coreapi.ChargeReq{
+			PaymentType: "qris",
+			TransactionDetails: midtrans.TransactionDetails{
+				OrderID:  kodePembayaran,
+				GrossAmt: int64(totalTarif),
+			},
+			CustomExpiry: &coreapi.CustomExpiry{
+				ExpiryDuration: 12,
+				Unit:           "hour",
+			},
+		}
+		coreApiRes, _ := c.ChargeTransaction(chargeReq)
+		return coreApiRes
+	}
+	return &coreapi.ChargeResponse{}
+}
+
+// get status of transaction that already recorded on midtrans (already `charge`-ed)
+func CheckStatusPayment(kodePembayaran string) *coreapi.TransactionStatusResponse {
+	c := MidtransCoreAPIClient()
+
+	res, _ := c.CheckTransaction(kodePembayaran)
+	return res
+}

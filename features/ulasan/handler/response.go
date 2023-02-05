c89d633e86e7287a8f4a497ca257ab138bd7851c
Eka Cahya Putra <ekacahyaps@gmail.com>
2023-02-05 23:39:08 +0700

    chore: add response for add ulasan, show ulasan by id guru, and show all ulasan

--- features/ulasan/handler/response.go
+++ features/ulasan/handler/response.go
@@ -1,1 +1,54 @@
 package handler
+
+import "Gurumu/features/ulasan"
+
+type UlasanGuruResponse struct {
+	ID        uint    `json:"id"`
+	NamaSiswa string  `json:"nama_siswa"`
+	Penilaian float32 `json:"penilaian"`
+	Ulasan    string  `json:"ulasan"`
+}
+
+type AllUlasanResponse struct {
+	ID        uint    `json:"id"`
+	NamaGuru  string  `json:"nama_guru"`
+	Penilaian float32 `json:"penilaian"`
+	Ulasan    string  `json:"ulasan"`
+}
+
+func UlasanGuruToResponse(dataCore ulasan.Core) UlasanGuruResponse {
+	return UlasanGuruResponse{
+		ID:        dataCore.ID,
+		NamaSiswa: dataCore.NamaSiswa,
+		Penilaian: dataCore.Penilaian,
+		Ulasan:    dataCore.Ulasan,
+	}
+}
+
+func ListUlasanGuruToResponse(dataCore []ulasan.Core) []UlasanGuruResponse {
+	var DataResponse []UlasanGuruResponse
+
+	for _, value := range dataCore {
+		DataResponse = append(DataResponse, UlasanGuruToResponse(value))
+	}
+	return DataResponse
+}
+
+//All Ulasan
+func AllUlasanToResponse(dataCore ulasan.Core) AllUlasanResponse {
+	return AllUlasanResponse{
+		ID:        dataCore.ID,
+		NamaGuru:  dataCore.NamaGuru,
+		Penilaian: dataCore.Penilaian,
+		Ulasan:    dataCore.Ulasan,
+	}
+}
+
+func ListAllUlasanToResponse(dataCore []ulasan.Core) []AllUlasanResponse {
+	var DataResponse []AllUlasanResponse
+
+	for _, value := range dataCore {
+		DataResponse = append(DataResponse, AllUlasanToResponse(value))
+	}
+	return DataResponse
+}

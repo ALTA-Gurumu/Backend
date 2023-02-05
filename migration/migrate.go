955b09046659a9ad7b60c8ac123bbff3a8ead74a
Eka Cahya Putra <ekacahyaps@gmail.com>
2023-02-05 23:40:46 +0700

    chore: add migration of ulasan

--- migration/migrate.go
+++ migration/migrate.go
@@ -4,6 +4,7 @@ import (
 	_guruData "Gurumu/features/guru/data"
 	_jadwaldata "Gurumu/features/jadwal/data"
 	"Gurumu/features/siswa/data"
+	_ulasandata "Gurumu/features/ulasan/data"
 
 	"gorm.io/gorm"
 )
@@ -12,4 +13,5 @@ func Migrate(db *gorm.DB) {
 	db.AutoMigrate(data.Siswa{})
 	db.AutoMigrate(_guruData.Guru{})
 	db.AutoMigrate(_jadwaldata.Jadwal{})
+	db.AutoMigrate(_ulasandata.Ulasan{})
 }

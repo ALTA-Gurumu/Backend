0f88aed8e9085f9e25433a706788d4dafad89afa
Eka Cahya Putra <ekacahyaps@gmail.com>
2023-02-05 23:38:17 +0700

    chore: add request for ulasan

--- features/ulasan/handler/request.go
+++ features/ulasan/handler/request.go
@@ -1,1 +1,22 @@
 package handler
+
+import "Gurumu/features/ulasan"
+
+type UlasanRegisterRequest struct {
+	Ulasan    string  `json:"ulasan" form:"ulasan"`
+	Penilaian float32 `json:"penilaian" form:"penilaian"`
+}
+
+func ToCore(data interface{}) *ulasan.Core {
+	res := ulasan.Core{}
+
+	switch data.(type) {
+	case UlasanRegisterRequest:
+		cnv := data.(UlasanRegisterRequest)
+		res.Ulasan = cnv.Ulasan
+		res.Penilaian = cnv.Penilaian
+	default:
+		return nil
+	}
+	return &res
+}

822caf2772e37c59b0850860ede45bbdbcc4adfa
Eka Cahya Putra <ekacahyaps@gmail.com>
2023-02-09 19:24:53 +0700

    fix: remove ID guru form Get All ulasan

--- features/ulasan/data/query.go
+++ features/ulasan/data/query.go
@@ -31,7 +31,7 @@ func (uq *ulasanQuery) Add(siswaId, guruId uint, newUlasan ulasan.Core) error {
 }
 func (uq *ulasanQuery) GetAll() ([]ulasan.Core, error) {
 	res := []AllUlasan{}
-	err := uq.db.Raw("SELECT ulasans.id, ulasans.guru_id, gurus.nama as nama_guru, ulasans.penilaian, ulasans.ulasan FROM ulasans JOIN gurus ON ulasans.guru_id = gurus.id WHERE ulasans.deleted_at is NULL").Find(&res).Error
+	err := uq.db.Raw("SELECT ulasans.id, gurus.nama as nama_guru, ulasans.penilaian, ulasans.ulasan FROM ulasans JOIN gurus ON ulasans.guru_id = gurus.id WHERE ulasans.deleted_at is NULL").Find(&res).Error
 	if err != nil {
 		log.Println("all ulasan query error")
 		return []ulasan.Core{}, err

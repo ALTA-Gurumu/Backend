7987ec7c3f92ec2165a00f9a44385df8421214d9
Herdiladania <herdiladania11@gmail.com>
2023-02-09 15:57:54 +0700

    testing : add case server problem autentikasi

--- features/autentikasi/service/service_test.go
+++ features/autentikasi/service/service_test.go
@@ -76,4 +76,25 @@ func TestLogin(t *testing.T) {
 		data.AssertExpectations(t)
 	})
 
+	t.Run("password salah", func(t *testing.T) {
+		data.On("Login", inputDataSiswa.Email).Return(expectedSiswa, nil)
+
+		srv := New(data)
+		token, res, err := srv.Login(inputDataSiswa.Email, "be1423")
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "password")
+		assert.Empty(t, token)
+		assert.Equal(t, uint(0), res.ID)
+		data.AssertExpectations(t)
+	})
+
+	t.Run("internal server error", func(t *testing.T) {
+		data.On("Login", inputDataSiswa.Email).Return(autentikasi.Core{}, errors.New("server error")).Once()
+		_, res, err := srv.Login(inputDataSiswa.Email, "hannn123")
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "server")
+		assert.Empty(t, nil)
+		assert.Equal(t, uint(0), res.ID)
+		data.AssertExpectations(t)
+	})
 }

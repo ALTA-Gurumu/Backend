13b9390208129bacafdfdb92300f66a50342bbcb
Herdiladania <herdiladania11@gmail.com>
2023-02-12 17:09:51 +0700

    feat : setup callback ouath

--- features/reservasi/service/service.go
+++ features/reservasi/service/service.go
@@ -6,10 +6,8 @@ import (
 	"errors"
 	"log"
 	"strings"
-	"time"
 
 	"github.com/go-playground/validator"
-	"google.golang.org/api/calendar/v3"
 )
 
 type reservasiService struct {
@@ -26,23 +24,6 @@ func New(rd reservasi.ReservasiData) reservasi.ReservasiService {
 func (rs *reservasiService) Add(token interface{}, newReservasi reservasi.Core) (reservasi.Core, error) {
 	siswaID := helper.ExtractToken(token)
 	res, err := rs.qry.Add(uint(siswaID), newReservasi)
-
-	// //contoh panggil fungsi
-	// helper.CreateEvent(
-	// 	&calendar.Event{
-	// 		Summary:     "Test Event",
-	// 		Location:    "Somewhere",
-	// 		Description: "This is a test event.",
-	// 		Start: &calendar.EventDateTime{
-	// 			DateTime: time.Now().Format(time.RFC3339),
-	// 			TimeZone: "Asia/Jakarta",
-	// 		},
-	// 		End: &calendar.EventDateTime{
-	// 			DateTime: time.Now().Add(time.Hour * 2).Format(time.RFC3339),
-	// 			TimeZone: "Asia/Jakarta",
-	// 		},
-	// 	})
-
 	if err != nil {
 		msg := ""
 		if strings.Contains(err.Error(), "not found") {
@@ -72,61 +53,6 @@ func (rs *reservasiService) Mysession(token interface{}, role, reservasiStatus s
 
 }
 
-// CallbackMid implements reservasi.ReservasiService
-func (rs *reservasiService) CallbackMid(kode string) error {
-	statusBayar := "Terbayar"
-
-	err := rs.qry.UpdateDataByTrfID(kode, reservasi.Core{
-
-		StatusPembayaran: statusBayar,
-	})
-
-	if err != nil {
-		msg := ""
-		if strings.Contains(err.Error(), "not found") {
-			msg = "data tidak ditemukan"
-		} else {
-			msg = "internal server error"
-		}
-		return errors.New(msg)
-	}
-
-	helper.CreateEvent(
-		&calendar.Event{
-			Summary:     "Konsultasi gurumu",
-			Location:    "",
-			Description: "matematika.",
-			ConferenceData: &calendar.ConferenceData{
-				CreateRequest: &calendar.CreateConferenceRequest{
-					RequestId: "sfsfs",
-					ConferenceSolutionKey: &calendar.ConferenceSolutionKey{
-						Type: "hangoutsMeet"},
-					Status: &calendar.ConferenceRequestStatus{
-						StatusCode: "success"},
-				}},
-
-			Start: &calendar.EventDateTime{
-				DateTime: time.Now().Add(time.Hour * 1).Format(time.RFC3339),
-				TimeZone: "Asia/Jakarta",
-			},
-			End: &calendar.EventDateTime{
-				DateTime: time.Now().Add(time.Hour * 4).Format(time.RFC3339),
-				TimeZone: "Asia/Jakarta",
-			},
-			Attendees: []*calendar.EventAttendee{
-				{Email: "herdiladania11@gmail.com"},
-				{Email: "sucinascaisar@gmail.com"},
-			},
-			Reminders: &calendar.EventReminders{
-				UseDefault: true,
-				// Overrides: []*calendar.EventReminder{
-				// 	{Method: "email", Minutes: 10},
-				// },
-			},
-		})
-
-	return nil
-}
 func (rs *reservasiService) NotificationTransactionStatus(kodeTransaksi string) error {
 
 	paymentStatus, err := helper.CheckStatusPayment(kodeTransaksi)

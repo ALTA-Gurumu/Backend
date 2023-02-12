550d8ee87e7f580dc6bfcbfdae720e5f6a1dd5aa
Herdiladania <herdiladania11@gmail.com>
2023-02-12 17:11:09 +0700

    feat : set evnt, move client secret to env

--- helper/calendar.go
+++ helper/calendar.go
@@ -1,12 +1,15 @@
 package helper
 
 import (
+	"Gurumu/config"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -15,7 +18,7 @@ import (
 )
 
 func GetClient(config *oauth2.Config) *http.Client {
-	tokFile := "features/reservasi/credentials/token.json"
+	tokFile := "./helper/temporary/token.json"
 	tok, err := tokenFromFile(tokFile)
 	if err != nil {
 		tok = getTokenFromWeb(config)
@@ -52,7 +55,6 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	return tok, err
 }
 
-// Saves a token to a file path.
 func SaveToken(path string, token *oauth2.Token) {
 	fmt.Printf("Saving credential file to: %s\n", path)
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
@@ -63,37 +65,70 @@ func SaveToken(path string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
-// client_id := os.Getenv("GOOGLE_OAUTH_CLIENT_ID1")
-// project := os.Getenv("GOOGLE_PROJECT_ID1")
-// secret := os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET1")
-// b := `{"installed":{"client_id":"` + client_id + `","project_id":"` + project + `","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"` + secret + `","redirect_uris":["http://localhost"]}}`
-// bt := []byte(b)
+func CalendarEvent(emailGuru, emailSiswa, pelajaran, tanggal, jam string) (string, error) {
 
-func CreateEvent(event *calendar.Event) {
-	// Baca token JSON
-	ctx := context.Background()
-	b, err := os.ReadFile("credentials.json")
+	layout := "2006-01-02 15:04:05"
+	value := tanggal + " " + jam + ":00"
+	dateTime, err := time.Parse(layout, value)
 	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
+		return "", errors.New("failed convert datetime")
+	}
+	setEvent := &calendar.Event{
+		Summary:     "Gurumu - Kelas " + pelajaran + "anda",
+		Description: "Kelas akan berlangsung pada " + tanggal + " pada " + jam + ". Harap datang tepat waktu dan pastikan untuk bergabung dengan panggilan video tepat waktu.",
+		ConferenceData: &calendar.ConferenceData{
+			CreateRequest: &calendar.CreateConferenceRequest{
+				RequestId: "sfsfs",
+				ConferenceSolutionKey: &calendar.ConferenceSolutionKey{
+					Type: "hangoutsMeet"},
+				Status: &calendar.ConferenceRequestStatus{
+					StatusCode: "success"},
+			}},
+		Start: &calendar.EventDateTime{
+			DateTime: dateTime.Format(time.RFC3339),
+			TimeZone: "Asia/Jakarta",
+		},
+		End: &calendar.EventDateTime{
+			DateTime: dateTime.Add(time.Hour * 1).Format(time.RFC3339),
+			TimeZone: "Asia/Jakarta",
+		},
+		Attendees: []*calendar.EventAttendee{
+			{Email: emailGuru},
+			{Email: emailSiswa},
+		},
+		Reminders: &calendar.EventReminders{
+			UseDefault: true,
+		},
 	}
 
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, calendar.CalendarEventsScope, calendar.CalendarScope, calendar.CalendarReadonlyScope)
+	ctx := context.Background()
+
+	client_id := config.GOOGLE_OAUTH_CLIENT_ID1
+	project := config.GOOGLE_PROJECT_ID1
+	secret := config.GOOGLE_OAUTH_CLIENT_SECRET1
+	b := `{"web":{"client_id":"` + client_id + `","project_id":"` + project + `","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"` + secret + `","redirect_uris":["http://localhost:8000/callback"]}}`
+	fmt.Println(b)
+	bt := []byte(b)
+	fmt.Println(bt)
+
+	config, err := google.ConfigFromJSON(bt, calendar.CalendarEventsScope, calendar.CalendarScope, calendar.CalendarReadonlyScope)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
 	client := GetClient(config)
-
 	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Calendar client: %v", err)
 	}
 
 	calendarId := "primary"
-	event, err = srv.Events.Insert(calendarId, event).SendUpdates("all").ConferenceDataVersion(1).Do()
+	resEvent, err := srv.Events.Insert(calendarId, setEvent).SendUpdates("all").ConferenceDataVersion(1).Do()
+
 	if err != nil {
 		log.Fatalf("Unable to create event. %v\n", err)
 	}
-	fmt.Printf("Event created: %s\n", event.HtmlLink)
-	fmt.Println(event.ConferenceData.ConferenceId)
+	fmt.Printf("Event created: %s\n", resEvent.HtmlLink)
+	fmt.Println(resEvent.ConferenceData.ConferenceId)
+
+	return resEvent.HtmlLink, nil
 }

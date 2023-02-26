1eb46f5a7c7183cd0bd6b98da2dc07d95b139394
Herdiladania <herdiladania11@gmail.com>
2023-02-27 01:22:21 +0700

    feat : fix duplicate jadwals

--- features/jadwal/data/query.go
+++ features/jadwal/data/query.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"Gurumu/features/jadwal"
+	"errors"
 	"log"
 
 	"gorm.io/gorm"
@@ -19,9 +20,18 @@ func New(db *gorm.DB) jadwal.JadwalData {
 
 func (jq *jadwalQuery) Add(guruID uint, newJadwal jadwal.Core) (jadwal.Core, error) {
 	data := CoreToData(newJadwal)
+
+	existedJadwal := Jadwal{}
+	err := jq.db.Where("id = ? AND tanggal = ? AND jam = ?", guruID, data.Tanggal, data.Jam).Find(&existedJadwal).Error
+	if err != nil {
+		return jadwal.Core{}, err
+	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
+		return jadwal.Core{}, err
+	}
+
 	data.GuruID = guruID
 	data.Status = "Tersedia"
-	err := jq.db.Create(&data).Error
+	err = jq.db.Create(&data).Error
 	if err != nil {
 		log.Println("error saat query tambah jadwal")
 		return jadwal.Core{}, err
